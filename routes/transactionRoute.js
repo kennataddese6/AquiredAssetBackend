3ef9890bec049e69d316ab7d9c90c4e2530057fc
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-13 12:19:53 +0300

    add create Transaction to object

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const createTransaction = require("../controllers/transactionController");
+const { createTransaction } = require("../controllers/transactionController");
 router.post("/", createTransaction);
 
 module.exports = router;
