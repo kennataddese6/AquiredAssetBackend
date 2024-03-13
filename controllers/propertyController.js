48f6a4a8cbd11eb4ba8a2d500bd9860632f424cf
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-13 14:01:57 +0300

    create a function for registering property assets

---
+++ controllers/propertyController.js
@@ -0,0 +1,39 @@
+const Property = require("../models/propertyModel");
+const asyncHandler = require("express-async-handler");
+
+const registerPropery = asyncHandler(async (req, res) => {
+  const property = await Property.create({
+    PropertyType: req.body.PropertyType,
+    CollateralType: req.body.CollateralType,
+    LHCNumber: req.body.LHCNumber,
+    ChassisNumber: req.body.ChassisNumber,
+    EngineNumber: req.body.EngineNumber,
+    ManufactureYear: req.body.ManufactureYear,
+    ModelNumber: req.body.ModelNumber,
+    SerialNumber: req.body.SerialNumber,
+    UniqueParcelNumber: req.body.UniqueParcelNumber,
+    ParcelArea: req.body.ParcelArea,
+    OwnerBorrowerName: req.body.OwnerBorrowerName,
+    AddressInBank: req.body.AddressInBank,
+    AcquisitionDate: req.body.AcquisitionDate,
+    AcquisitionValue: req.body.AcquisitionValue,
+    PrincipaleAcquistion: req.body.PrincipaleAcquistion,
+    InterestAcquisition: req.body.InterestAcquisition,
+    FurnitureList: req.body.FurnitureList,
+    Disposed: req.body.Disposed,
+    Disposal: req.body.Disposal,
+    ReEstimation: req.body.ReEstimation,
+    InsuranceRenewal: req.body.InsuranceRenewal,
+    PropertyAddress: req.body.PropertyAddress,
+    Document: req.body.Document,
+  });
+  if (property) {
+    res.status(200).json(property);
+  } else {
+    res.status(400);
+  }
+});
+
+module.exports = {
+  registerPropery,
+};
