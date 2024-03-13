3806f96f107ac64563327a7f7053738d935449fd
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-13 14:20:27 +0300

    use route for property

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 app.use("/api/transactions", require("./routes/transactionRoute"));
+app.use("/api/property", require("./routes/propertyRoutes"));
 
 const server = app.listen(port, () =>
   console.log(`Server started on port ${port}`)
