e8f2bb73a631f024baaa0b8b2b8e2ef1dde77b34
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-13 11:24:33 +0300

    add a schema for transactions

---
+++ models/transactionModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = mongoose.Schema({
+  PropertyId: {
+    type: String,
+    require: [true, "Please add a Property Id"],
+  },
+  TransactionCategory: {
+    type: String,
+    require: [true, "please add a Transaction Category"],
+  },
+  TransactionType: {
+    type: String,
+    require: [true, "please add a Transaction type"],
+  },
+  TransactionDate: {
+    type: Date,
+    require: [true, "please add a Transaction Date"],
+  },
+  TransactionValue: {
+    type: Number,
+    require: [true, "please add a Transaction Value"],
+  },
+});
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
