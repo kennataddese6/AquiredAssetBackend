8295f1e137f230a5baf054c56fef9ce2eaca5f9a
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-14 10:08:52 +0300

    add a route for getting all properties

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerProperty } = require("../controllers/propertyController");
-router.post("/", registerProperty);
+const {
+  registerProperty,
+  getAllProperty,
+} = require("../controllers/propertyController");
+
+router.post("/", registerProperty).get(getAllProperty);
 
 module.exports = router;
