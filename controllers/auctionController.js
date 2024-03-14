8c5871f9e4d11a1a2e2192927788e204fb53a0e3
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-14 09:53:48 +0300

    create a auction controller and function to create auctions

---
+++ controllers/auctionController.js
@@ -0,0 +1,24 @@
+const Auction = require("../models/auctionModel");
+const asyncHandler = require("express-async-handler");
+
+const createAuction = asyncHandler(async (req, res) => {
+  const auction = await Auction.create({
+    PropertyId: req.body.PropertyId,
+    AuctionMinimalPrice: req.body.AuctionMinimalPrice,
+    AuctionSubmissionDate: req.body.AuctionSubmissionDate,
+    AuctionOpeningDate: req.body.AuctionOpeningDate,
+    AuctionResult: req.body.AuctionResult,
+    AuctionSellPrice: req.body.AuctionSellPrice,
+    AuctionProfitValue: req.body.AuctionProfitValue,
+    AuctionLossValue: req.body.AuctionLossValue,
+  });
+  if (auction) {
+    res.status(200).json(auction);
+  } else {
+    res.status(400);
+  }
+});
+
+module.exports = {
+  createAuction,
+};
