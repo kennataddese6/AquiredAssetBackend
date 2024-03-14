76114b438bbf6a117a63a344b4cf9cfa0a72a727
Kenna Taddese Roba <KennaTaddese@cbe.com.et>
2024-03-14 10:04:50 +0300

    create route for getting all auctions

--- routes/auctionRoutes.js
+++ routes/auctionRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createAuction } = require("../controllers/auctionController");
+const {
+  createAuction,
+  getAllAuctions,
+} = require("../controllers/auctionController");
 
 router.post("/", createAuction);
+router.get("/", getAllAuctions);
 
 module.exports = router;
