19a72a3c1561717bcd047712ddde23c92362835c
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-04 16:30:49 +0300

    remove required from property attributes from property schema

--- models/propertyModel.js
+++ models/propertyModel.js
@@ -2,178 +2,178 @@ const mongoose = require("mongoose");
 const DisposalSchema = new mongoose.Schema({
   DisposalType: {
     type: String,
-    required: [true, "Please add a Postpone value"],
+    // required: [true, "Please add a Postpone value"],
   },
   IncomeGenerated: {
     type: String,
-    required: [true, "Please add an IncomeGenereated"],
+    // required: [true, "Please add an IncomeGenereated"],
   },
 });
 const EstimationSchema = new mongoose.Schema({
   EstimationDate: {
     type: Date,
-    required: [true, "Please add a Estimation Date"],
+    // required: [true, "Please add a Estimation Date"],
   },
   EstimationValue: {
     type: String,
-    required: [true, "Please add a Estimation value"],
+    // required: [true, "Please add a Estimation value"],
   },
 });
 const InsuranceRenewalSchema = new mongoose.Schema({
   RenewalStartDate: {
     type: Date,
-    required: [true, "Please add an Insurance Renewal Start Date"],
+    // required: [true, "Please add an Insurance Renewal Start Date"],
   },
   RenewalEndDate: {
     type: Date,
-    required: [true, "Please add an Insurance Renewal End Date"],
+    // required: [true, "Please add an Insurance Renewal End Date"],
   },
 });
 const DocumentSchema = new mongoose.Schema({
   Document: {
     type: Buffer,
-    required: [true, "Please Upload a document"],
+    // required: [true, "Please Upload a document"],
   },
   DocumentType: {
     type: String,
-    required: [true, "Please add a Document type"],
+    // required: [true, "Please add a Document type"],
   },
   RefNo: {
     type: Date,
-    required: [true, "Please add a Reference Number"],
+    // required: [true, "Please add a Reference Number"],
   },
   Date: {
     type: Date,
-    required: [true, "Please add a Date"],
+    // required: [true, "Please add a Date"],
   },
 });
 const AddressSchema = new mongoose.Schema({
   Region: {
     type: String,
-    required: [true, "Please add a Region"],
+    // required: [true, "Please add a Region"],
   },
   CityAdmin: {
     type: String,
-    required: [true, "Please add a CIty Admin"],
+    // required: [true, "Please add a CIty Admin"],
   },
   Zone: {
     type: String,
-    required: [true, "Please add a Zone"],
+    // required: [true, "Please add a Zone"],
   },
   City: {
     type: String,
-    required: [true, "Please add a CIty"],
+    // required: [true, "Please add a CIty"],
   },
   SubCity: {
     type: String,
-    required: [true, "Please add a SubCity"],
+    // required: [true, "Please add a SubCity"],
   },
   Woreda: {
     type: String,
-    required: [true, "Please add a Woreda"],
+    // required: [true, "Please add a Woreda"],
   },
   Kebele: {
     type: String,
-    required: [true, "Please add a Kebele"],
+    // required: [true, "Please add a Kebele"],
   },
   HouseNo: {
     type: String,
-    required: [true, "Please add a House Number"],
+    // required: [true, "Please add a House Number"],
   },
 });
 const PropertySchema = mongoose.Schema(
   {
     PropertyType: {
       type: String,
-      required: [true, "Please add a Property type"],
+      // required: [true, "Please add a Property type"],
     },
     CollateralType: {
       type: String,
-      required: [true, "Please add a CollateralType"],
+      // required: [true, "Please add a CollateralType"],
     },
     LHCNumber: {
       type: String,
-      required: [true, "Please add an LHC Number"],
+      // required: [true, "Please add an LHC Number"],
     },
     ChassisNumber: {
       type: String,
-      required: [true, "Please add a Chassis Number"],
+      // required: [true, "Please add a Chassis Number"],
     },
     EngineNumber: {
       type: String,
-      required: [true, "Please add Engine Number"],
+      // required: [true, "Please add Engine Number"],
     },
     ManufactureYear: {
       type: String,
-      required: [true, "Please add a Manufacture Year"],
+      // required: [true, "Please add a Manufacture Year"],
     },
     ModelNumber: {
       type: String,
-      required: [true, "Please add a Model Number"],
+      // required: [true, "Please add a Model Number"],
     },
     SerialNumber: {
       type: String,
-      required: [true, "Please add a Serial Number"],
+      // required: [true, "Please add a Serial Number"],
     },
     UniqueParcelNumber: {
       type: Number,
-      required: [true, "Please add a Unique Parcel Number"],
+      // required: [true, "Please add a Unique Parcel Number"],
     },
     ParcelArea: {
       type: Number,
-      required: [true, "Please add a Parcel Area"],
+      // required: [true, "Please add a Parcel Area"],
     },
     OwnerBorrowerName: {
       type: String,
-      required: [true, "Please add an Owner/Borrower Name"],
+      // required: [true, "Please add an Owner/Borrower Name"],
     },
     AddressInBank: {
       type: String,
-      required: [true, "Please add an Address In Bank"],
+      // required: [true, "Please add an Address In Bank"],
     },
     AcquisitionDate: {
       type: Date,
-      required: [true, "Please add a Acquisition Date"],
+      // required: [true, "Please add a Acquisition Date"],
     },
     AcquisitionValue: {
       type: Boolean,
-      required: [true, "Please add an Acquisition Value"],
+      // required: [true, "Please add an Acquisition Value"],
     },
     PrincipaleAcquistion: {
       type: String,
-      required: [true, "Please add a Principale Acquistion"],
+      // required: [true, "Please add a Principale Acquistion"],
     },
     InterestAcquisition: {
       type: String,
-      required: [true, "Please add an Interest Acquisition"],
+      // required: [true, "Please add an Interest Acquisition"],
     },
     FurnitureList: {
       type: [Object],
-      required: [true, "Please add a  Furniture List"],
+      // required: [true, "Please add a  Furniture List"],
     },
     Disposed: {
       type: Boolean,
-      required: [true, "Please add a Disposal Status"],
+      // required: [true, "Please add a Disposal Status"],
     },
     Disposal: {
       type: DisposalSchema,
-      required: [true, "Please add a Disposal"],
+      // required: [true, "Please add a Disposal"],
     },
     ReEstimation: {
       type: EstimationSchema,
-      required: [true, "Please add a Re-estimation"],
+      // required: [true, "Please add a Re-estimation"],
     },
     InsuranceRenewal: {
       type: InsuranceRenewalSchema,
-      required: [true, "Please add Insurance Renewal"],
+      // required: [true, "Please add Insurance Renewal"],
     },
     PropertyAddress: {
       type: AddressSchema,
-      required: [true, "Please add a Property Address"],
+      // required: [true, "Please add a Property Address"],
     },
     Document: {
       type: DocumentSchema,
-      required: [true, "Please add a Document"],
+      // required: [true, "Please add a Document"],
     },
   },
   {
