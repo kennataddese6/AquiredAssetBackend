1a9739ff99a0a4a07ad7c7ad9e185647fca59084
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-08 12:04:30 +0300

    create a route for the new upload documents function

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "C://Users/KennaTaddese.CBE/Documents");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
+const upload = multer({ storage: storage });
 const {
   registerProperty,
   getAllProperty,
+  uploadDocument,
 } = require("../controllers/propertyController");
 
 router.route("/").post(registerProperty).get(getAllProperty);
 
+router.post("/document", upload.single("file"), uploadDocument);
 module.exports = router;
