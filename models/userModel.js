418f9192afccdb29c5eedd7608d1f56e8fa7883d
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-04-19 10:55:04 +0300

    add secretkey to env

--- models/userModel.js
+++ models/userModel.js
@@ -1,31 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Please add a name'],
     },
     secretKey: {
       type: String,
-      default: 'sDoVwRLhojZxeyQICmqhjPulH4tTNviziSu'
+      default: process.env.SECRET_KEY,
     },
     roles: {
-      type: Number,
+      type: String,
     },
     email: {
       type: String,
-      required: [true, 'Please add an email'],
       unique: true,
     },
     password: {
       type: String,
-      required: [true, 'Please add a password'],
     },
   },
   {
     timestamps: true,
-  },
+  }
 );
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
