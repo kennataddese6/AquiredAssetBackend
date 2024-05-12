ea952fc7b0a535edc94ffd76c1b7dc47a0ab934e
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-12 16:30:35 +0300

    validate files properties

--- middleware/validator.js
+++ middleware/validator.js
@@ -1,20 +1,44 @@
-const { body } = require("express-validator");
+const { body, param, check } = require("express-validator");
 
-function createValidator(field, value) {
+function createValidator(field, value, location) {
+  let validator;
+  switch (location) {
+    case "body":
+      validator = body(field);
+      break;
+    case "params":
+      validator = param(field);
+      break;
+    case "file":
+      validator = check(`file.${field}`);
+      break;
+  }
   if (typeof value === "object" && value !== null && !Array.isArray(value)) {
     return Object.entries(value).flatMap(([nestedField, nestedValue]) =>
-      createValidator(`${field}.${nestedField}`, nestedValue)
+      createValidator(`${field}.${nestedField}`, nestedValue, location)
     );
   }
-  return body(field).trim().escape();
+  return validator.trim().escape();
 }
 
 exports.validator = (req, res, next) => {
-  const validations = Object.entries(req.body).flatMap(([field, value]) =>
-    createValidator(field, value)
+  const bodyValidations = Object.entries(req.body).flatMap(([field, value]) =>
+    createValidator(field, value, "body")
   );
+  const paramValidations = Object.entries(req.params).flatMap(
+    ([field, value]) => createValidator(field, value, "params")
+  );
+  const fileValidations = req.file
+    ? Object.entries(req.file).flatMap(([field, value]) =>
+        createValidator(field, value, "file")
+      )
+    : [];
 
-  Promise.all(validations.map((validation) => validation.run(req))).then(() => {
+  Promise.all(
+    [...bodyValidations, ...paramValidations, ...fileValidations].map(
+      (validation) => validation.run(req)
+    )
+  ).then(() => {
     next();
   });
 };
