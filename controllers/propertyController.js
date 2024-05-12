0c73b2983ef9552d369d9ee451a70d574625b66e
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-12 15:47:38 +0300

    simplifiy creating of property

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -2,33 +2,16 @@ const Property = require("../models/propertyModel");
 const asyncHandler = require("express-async-handler");
 const Document = require("../models/documentModel");
 const transactionInfo = require("../transactionInfo.json");
+
 const registerProperty = asyncHandler(async (req, res) => {
-  const property = await Property.create({
-    PropertyType: req.body.PropertyType,
-    CollateralType: req.body.CollateralType,
-    LHCNumber: req.body.LHCNumber,
-    ChassisNumber: req.body.ChassisNumber,
-    EngineNumber: req.body.EngineNumber,
-    ManufactureYear: req.body.ManufactureYear,
-    ModelNumber: req.body.ModelNumber,
-    SerialNumber: req.body.SerialNumber,
-    UniqueParcelNumber: req.body.UniqueParcelNumber,
-    ParcelArea: req.body.ParcelArea,
-    OwnerBorrowerName: req.body.OwnerBorrowerName,
-    AddressInBank: req.body.AddressInBank,
-    AcquisitionDate: req.body.AcquisitionDate,
-    AcquisitionValue: req.body.AcquisitionValue,
-    PrincipaleAcquistion: req.body.PrincipaleAcquistion,
-    InterestAcquisition: req.body.InterestAcquisition,
-    FurnitureList: req.body.FurnitureList,
-    Disposed: req.body.Disposed,
-    Disposal: req.body.Disposal,
-    ReEstimation: req.body.ReEstimation,
-    InsuranceRenewal: [req.body.InsuranceRenewal],
-    PropertyAddress: req.body.PropertyAddress,
-    BranchName: req.body.BranchName,
-    DistrictName: req.body.DistrictName,
-  });
+  const propertyData = { ...req.body };
+  if (
+    propertyData.InsuranceRenewal &&
+    !Array.isArray(propertyData.InsuranceRenewal)
+  ) {
+    propertyData.InsuranceRenewal = [propertyData.InsuranceRenewal];
+  }
+  const property = await Property.create(propertyData);
   if (property) {
     res.status(200).json(property);
   } else {
