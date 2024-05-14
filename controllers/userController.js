e6d543157e5dc2891885570194d900febd45b83d
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-14 18:07:48 +0300

    return user data after successful login

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,25 +5,20 @@ const login = asyncHandler(async (req, res) => {
   try {
     const { mail, password } = req.body;
     ad.findUser({ attributes: ["*"] }, mail, (err, user) => {
-      if (err) {
-        res.status(500).json({ message: "Error retrieving user data" });
-        return;
-      }
       if (user) {
-        ad.authenticate(mail, password, async (err, auth) => {
-          if (err) {
-            throw new Error(err);
-          }
-          if (!auth) {
-            throw new Error("Incorrect username or password");
-          } else if (user) {
+        ad.authenticate(user?.mail, password, async (err, auth) => {
+          if (auth) {
             res.status(200).json(user);
+          } else {
+            res.status(400).json("Incorrect username or password");
           }
         });
+      } else {
+        res.status(400).json("User not found");
       }
     });
   } catch (error) {
-    res.status(400).json(error);
+    console.log(error);
   }
 });
 
