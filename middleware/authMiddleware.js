4d73ec7bc4eb26b741c1f800b5f54411af6780c8
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-17 16:02:19 +0300

    add middle ware to verify branch and distrci name

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,36 +1,34 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const User = require("../models/userModel");
+const ad = require("../config/ad");
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      // Get token from header
-      token = req.headers.authorization.split(" ")[1];
-
-      // Verify token
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // Get user from the token
-      req.user = await User.findById(decoded.id).select("-password");
+  const token = req.cookies.token;
+  if (!token) {
+    res.status(401).json("Not authorized");
+    return;
+  }
+  const secret = process.env.JWT_SECRET || "ThisIsAVerySillyKeyToDo";
+  const decoded = jwt.verify(token, secret);
+  const mail = decoded.mail;
+  if (!mail) {
+    res.status(404).json("Something went wrong");
+    return;
+  }
+  ad.findUser({ attributes: ["*"] }, mail, (err, user) => {
+    if (user) {
+      const BranchName = user.department;
+      const DistrictName = user.company;
+      req.body.BranchName = BranchName;
+      req.body.DistrictName = DistrictName;
+      req.params.BranchName = BranchName;
+      req.params.DistrictName = DistrictName;
 
       next();
-    } catch (error) {
-      console.log(error);
-      res.status(401);
-      throw new Error("Not authorized");
+    } else {
+      res.status(400).json("User not found");
     }
-  }
-
-  if (!token) {
-    res.status(401);
-    throw new Error("Not authorized, no token");
-  }
+  });
 });
 
 module.exports = { protect };
