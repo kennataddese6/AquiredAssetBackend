1b61922399c4ca5765cd9619512d4f87b13041c3
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-18 10:43:00 +0300

    respond back if property not found

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -66,7 +66,7 @@ const getProperty = asyncHandler(async (req, res) => {
     };
     res.status(200).json(PropertyInfo);
   } else {
-    res.status(400);
+    res.status(404).json("Property not found!");
   }
 });
 
