0af82c85d47ad139d9cde50ca6e5d79ebe48a6f7
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-20 10:49:03 +0300

    protect get me

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 const { login, getMe } = require("../controllers/userController");
 const { validator } = require("../middleware/validator");
 
 router.route("/login").post(validator, login);
-router.route("/me").get(validator, getMe);
+router.route("/me").get(protect, validator, getMe);
 
 module.exports = router;
