6839f493f9af90e37479ace9bc4503ec89574d1c
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-21 17:31:03 +0300

    add route to user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,13 @@ const {
   getMe,
   findUser,
   logout,
+  createUser,
 } = require("../controllers/userController");
 const { validator } = require("../middleware/validator");
 
 router.route("/login").post(validator, login);
 router.route("/logout").post(logout);
-router.route("/").get(protect, findUser);
+router.route("/").get(protect, findUser).post(createUser);
 router.route("/me").get(protect, validator, getMe);
 
 module.exports = router;
