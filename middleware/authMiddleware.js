a2361e0143265725c1040b570bb4a915dde864a7
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-05-29 17:58:47 +0300

    refactor middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
-const ad = require("../config/ad");
 const User = require("../models/userModel");
+
 const protect = asyncHandler(async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
@@ -9,34 +9,13 @@ const protect = asyncHandler(async (req, res, next) => {
     return;
   }
   const secret = process.env.JWT_SECRET || "ThisIsAVerySillyKeyToDo";
-
   try {
-    const decoded = jwt.verify(token, secret);
-    const mail = decoded.mail;
-    if (!mail) {
-      res.status(404).json("Something went wrong");
-      return;
-    }
-    const localUser = await User.findOne({
-      mail: { $regex: new RegExp("^" + mail + "$", "i") },
+    req.user = await User.findOne({
+      mail: {
+        $regex: new RegExp("^" + jwt.verify(token, secret).mail + "$", "i"),
+      },
     });
-    if (localUser) {
-      req.body.role = localUser.role;
-    }
-    ad.findUser({ attributes: ["*"] }, mail, (err, user) => {
-      if (user) {
-        const BranchName = user.department;
-        const DistrictName = user.company;
-        req.body.BranchName = BranchName;
-        req.body.DistrictName = DistrictName;
-        req.params.BranchName = BranchName;
-        req.params.DistrictName = DistrictName;
-
-        next();
-      } else {
-        res.status(400).json("User not found");
-      }
-    });
+    next();
   } catch (err) {
     if (err instanceof jwt.TokenExpiredError) {
       res.status(401).json("Token has expired. Please log in again.");
