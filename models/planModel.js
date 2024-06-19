cb7689cb1585a451bfff3d665cadcdf28c7048f3
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-06-19 11:10:26 +0300

    add a plan schema

---
+++ models/planModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const TransactionSchema = mongoose.Schema(
+  {
+    PropertyId: {
+      type: String,
+      require: [true, "Please add a Property Id"],
+    },
+    Region: {
+      type: String,
+      require: [true, "Please add a Region"],
+    },
+    BranchName: {
+      type: String,
+      require: [true, "Please add a Property Branch Name"],
+    },
+    DistrictName: {
+      type: String,
+      require: [true, "Please add a Property District Name"],
+    },
+    Quarter: {
+      type: Number,
+      require: [true, "Please a Quarter"],
+    },
+    DisposedBy: {
+      type: String,
+      require: [true, "Plesae add a Disposal Mechanism"],
+    },
+    Year: {
+      type: Number,
+      require: [true, "Please add a year"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Transaction", TransactionSchema);
