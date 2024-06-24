1868f3d21e769cf9a882fe38327758f8281189db
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-06-24 14:23:39 +0300

    secure get property

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -5,6 +5,7 @@ const path = require("path");
 const fs = require("fs");
 const { validator } = require("../middleware/validator");
 const { protect } = require("../middleware/authMiddleware");
+const { secure } = require("../middleware/secureChannel");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const dir = path.join(__dirname, "../uploads");
@@ -43,7 +44,7 @@ router.route("/re-estimation/").post(addReEstimation);
 router.route("/re-estimation/:Id").get(getReEstimations);
 router.route("/branch/").get(validator, protect, getBranchProperty);
 router.route("/district/").get(validator, protect, getDistrictProperty);
-router.route("/:Id([0-9a-fA-F]{24})").get(validator, getProperty);
+router.route("/:Id([0-9a-fA-F]{24})").get(secure, validator, getProperty);
 router.post("/document", upload.single("file"), validator, uploadDocument);
 router.get("/document", getDocuments);
 router.get("/document/:Id", getDocument);
