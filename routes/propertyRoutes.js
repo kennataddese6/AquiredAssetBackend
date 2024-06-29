1ff4ee394c649bd24729f3dc1585a289064173da
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-06-29 10:10:16 +0300

    limit rate

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -20,7 +20,13 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+});
+
 const {
   registerProperty,
   getAllProperty,
