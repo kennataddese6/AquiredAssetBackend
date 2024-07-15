83c243ea9d81a867fd93c3162249fa8714439434
Kenna Taddese Roba <kennataddese6@gmail.com>
2024-07-15 09:08:06 +0300

    add db pass to env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const MONGO_URI = `mongodb+srv://Kenna:$${process.env.DB_PASS}@cluster0.jdtosny.mongodb.net/aquiredAsset`;
+    const MONGO_URI = `mongodb+srv://Kenna:${process.env.DB_PASS}@cluster0.jdtosny.mongodb.net/aquiredAsset`;
     const conn = await mongoose.connect(MONGO_URI, {
       family: 4,
     });
